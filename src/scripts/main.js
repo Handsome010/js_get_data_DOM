c36595464cfb0f11e9827f6686be2b46112e65d2
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-03 21:06:22 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const pop = [...document.body.querySelectorAll('span.population')];
+const values = [];
+
+for (const item of pop) {
+  values.push(+item.textContent.replaceAll(',', ''));
+}
+
+const sum = values.reduce((acc, item) => acc + item, 0);
+
+document.body.querySelector('.total').textContent =
+  'Total: ' + sum.toLocaleString('en-US');
+
+document.body.querySelector('.average').textContent =
+  'Average: ' + Math.round(sum / values.length).toLocaleString('en-US');
